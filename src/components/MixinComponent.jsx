c86cfc24eac35c32205a0cb4ea6aab35f455569d
hoisharka <hoisharka@gmail.com>
2018-01-04 19:41:07 +0900

    mixin

---
+++ src/components/MixinComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MixinComponent = () => (
+  <div className="container">
+    <h1>SASS Mixins</h1>
+    <p>A mixin lets you make groups of CSS declarations that you want to reuse throughout your site.</p>
+
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box box2"></div>
+    <div className="box box2"></div>
+  </div>
+);
+
+export default MixinComponent;
